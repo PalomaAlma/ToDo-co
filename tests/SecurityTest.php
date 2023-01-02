176bb3df86a635abf2cde8f56df50f631ef0307a
PalomaAlma <palomavacheron@gmail.com>
2023-01-02 15:31:36 +0100

    Security tests

---
+++ tests/SecurityTest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class SecurityTest extends WebTestCase
+{
+    public function testLoginIsSuccessful(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/login');
+        $csrfToken = $client->getContainer()->get('security.csrf.token_manager')->getToken('authenticate');
+
+        $form = $crawler->filter('form[name=login]')->form([
+            '_username' => 'admin',
+            '_password' => '123456',
+            '_csrf_token' => $csrfToken
+        ]);
+
+        $client->submit($form);
+
+        $this->assertResponseRedirects();
+
+        $client->followRedirect();
+        $this->assertResponseIsSuccessful();
+    }
+
+    public function testLogout(): void
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/logout');
+
+        $this->assertResponseRedirects();
+
+        $client->followRedirect();
+        $this->assertResponseIsSuccessful();
+    }
+}
